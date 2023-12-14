48f31d76ce64465642b40a3e03e99e70d754fc90
cablearora <cablearora@gmail.com>
2023-12-14 22:13:27 +0530

    "code committed"

--- webpage/home.py
+++ webpage/home.py
@@ -98,8 +98,8 @@ class Home_Page:
             elif option == "Select a table from Sample Data":
                 client = MongoClient("mongodb+srv://test:NRs2DqKb13fZ6AcY@cluster0.xgvrey3.mongodb.net/your_mongo_database?retryWrites=true&w=majority")
                 db = client['your_mongo_database']
-                st.write(db)
                 table_list = db.list_collection_names()
+                st.write(table_list)
                 table_name  = [list for list in table_list]
                 
                 selected_table = st.selectbox("Select a table", table_name)
